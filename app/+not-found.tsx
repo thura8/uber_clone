2255899dea8e782b4501e30b31d4c53f45712d06
Thu Ra Lynn Htun <lynnhtunthura8@gmail.com>
2025-05-04 21:13:23 +0630

    Modify some code and add assets folder

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,10 +1,16 @@
-import { Stack } from "expo-router";
-import { StyleSheet } from "react-native";
+import { Link, Stack } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: "Oops!" }} />
+      <View style={styles.container}>
+        <Text type="title">This screen doesn't exist.</Text>
+        <Link href={"/"} style={styles.link}>
+          <Text type="link">Go to home screen!</Text>
+        </Link>
+      </View>
     </>
   );
 }
